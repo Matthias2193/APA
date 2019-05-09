8e9145996444536d84779bb5a8d7b4ea16b809a0
Matthias2193 <matthias.becher2193@gmail.com>
2019-05-09 14:13:45 +0200

    Added two scripts without any content so far

---
+++ DecisionTreeImplementation.R
@@ -0,0 +1,2 @@
+#This script contains the implementation of the decision tree following the paper 'Decision trees for uplift modeling
+#with single and multiple treatments
---
+++ MainScript.R
@@ -0,0 +1,6 @@
+#The main script for data analysis and model evaluation
+
+
+#Data import
+email <- read.csv('Email.csv')
+insurance <- read.csv('Insurance.csv')
