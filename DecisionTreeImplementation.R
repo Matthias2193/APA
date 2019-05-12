05194639f49acdef7295663f09b5eec2e7d88bc1
Matthias2193 <matthias.becher2193@gmail.com>
2019-05-12 11:38:55 +0200

    Fixed 'select_split' method Added some preprocessing of the data

--- DecisionTreeImplementation.R
+++ DecisionTreeImplementation.R
@@ -16,11 +16,11 @@ library('philentropy')
 email$men_treatment <- ifelse(email$segment=='Mens E-Mail',1,0)
 email$women_treatment <- ifelse(email$segment=='Womens E-Mail',1,0)
 email$control <- ifelse(email$segment=='No E-Mail',1,0)
-
+email$segment <- NULL
 
 
 #Set up tests
-set_up_tests <- function(x){
+set_up_tests <- function(x,reduce_cases){
   type_list <- sapply(x, class)
   categorical_splits = list()
   numerical_splits = list()
@@ -34,7 +34,11 @@ set_up_tests <- function(x){
       s <- 2
       final_list <- c()
       while(r < length(temp_list)){
-        final_list <- c(final_list,(temp_list[r]+temp_list[s])/2)
+        final_list <- c(final_list,round((temp_list[r]+temp_list[s])/2,1))
+        final_list <- unique(final_list)
+        if((length(final_list)>1000)&& reduce_cases){
+          final_list <- round(final_list)
+        }
         r <- r+1
         s <- s+1
       }
@@ -51,23 +55,35 @@ set_up_tests <- function(x){
 select_split <- function(a,l,g,divergence,test_list,treatment,control,target,temp_data){
   gain_list <- c()
   name_list <- c()
-  for(t in test_list$categorical){
+  for(x in 1:length(test_list$categorical)){
+    t <- test_list$categorical[x]
     gain_list <- c(gain_list,gain(a,l,g,divergence,t[[1]],
                                   treatment,control,target,temp_data,'categorical',names(t)))
-    name_list <- c(name_list,t)
+    name_list <- c(name_list,names(t))
   }
-  for(t in test_list$numerical){
-    gain_list <- c(gain_list,gain(a,l,g,divergence,number_of_treatments,t[[1]],
-                                  treatment,control,target,temp_data,'numerical',names(t)))
-    name_list <- c(name_list,t)
+  for(x in 1:length(test_list$numerical)){
+    temp_name <- names(test_list$numerical[x]) 
+    for(y in 1:length(test_list$numerical[[x]])){
+      t <- test_list$numerical[[x]][y]
+      new_name <- paste(temp_name, as.character(t), sep = '_')
+      gain_list <- c(gain_list,gain(a,l,g,divergence,t,
+                                    treatment,control,target,temp_data,'numerical',temp_name))
+      name_list <- c(name_list,new_name)
+    }
   }
-  return(name_list[match(max(gain_list),gain_list)])
+  temp_string <- name_list[match(max(gain_list),gain_list)]
+  temp_result <- strsplit(temp_string,split='_', fixed=TRUE)
+  result <- c(as.numeric(temp_result[[1]][2]))
+  names(result) <- temp_result[[1]][1]
+  return(result)
 }
 
+
+
 gain <- function(a,l,g,divergence, test_case,treatment,control,target,temp_data,test_type,test_col){
-  conditional <- conditional_divergence(a,l,g,divergence,test_case,treatments,control,target,temp_data,
-                                         test_type,test_col)
-  multiple <- multiple_divergence(a,l,g,divergence,treatments,control,target,temp_data,test_col)
+  conditional <- conditional_divergence(a,l,g,divergence,test_case,treatment,control,target,temp_data,
+                                        test_type,test_col)
+  multiple <- multiple_divergence(a,l,g,divergence,treatment,control,target,temp_data)
   return(conditional-multiple)
 }
 
@@ -98,43 +114,52 @@ conditional_divergence <- function(a,l,g,divergence,test_case,treatments,control
     }
   }
   else{
+    t <- test_case
     div <- div + nrow(temp_data[temp_data[test_col]<t,])/nrow(temp_data)*
       multiple_divergence(a,l,g,divergence,treatments,control,target,
                           temp_data[temp_data[test_col]<t,])
-    div <- div + nrow(temp_data[temp_data[test_col]>t,])/nrow(temp_data)*
+    div <- div + nrow(temp_data[temp_data[test_col]>=t,])/nrow(temp_data)*
       multiple_divergence(a,l,g,divergence,treatments,control,target,
-                          temp_data[temp_data[test_col]>t,])
+                          temp_data[temp_data[test_col]>=t,])
   }
   return(div)
 }
 
 binary_KL_divergence <- function(x,y){
-  p <- mean(x)
-  q <- mean(y)
-  return((p*log(p/q))+((1-p)*log((1-p)/1-q)))
+  p <- sum(x)/length(x)
+  q <- sum(y)/length(y)
+  temp_result <- (p*log(p/q))+((1-p)*log((1-p)/1-q))
+  if(is.nan(temp_result)||is.infinite(temp_result)){
+    return(0)
+  }
+  else{
+    return(temp_result)
+  }
 }
 
 
 
 ####Test Area
 
-divergence <-'binary_KL_divergence'
-multiple <- 0
-a <- 1
-l<- c(0.5,0.5)
-g<- matrix(0.25,nrow = 2,ncol = 2)
-target <- 'visit'
+# divergence <-'binary_KL_divergence'
+# multiple <- 0
+# a <- 1
+# l<- c(0.5,0.5)
+# g<- matrix(0.25,nrow = 2,ncol = 2)
+# target <- 'visit'
 treatment_list <- c('men_treatment','women_treatment')
-test_list <- set_up_tests(email)
-control <- 'control'
-temp_test <- test_list$categorical[1]
-test_col <- names(temp_test)
-
-conditional_divergence(a,l,g,'binary_KL_divergence',temp_test,treatment_list,control,target,email,'categorical',
-                    names(temp_test))
-
+test_list <- set_up_tests(email[,c("recency","history_segment","history","mens","womens","zip_code",
+                                   "newbie","channel","spend")],TRUE)
+# control <- 'control'
+# test_case <- test_list$numerical[1]
+# test_col <- names(temp_test)
+# test_type <- 'numerical'
 
 
 
-select_split(a,l,g,'binary_KL_divergence',test_list,treatment_list,'control','visit',email)
+
+
+select_split(a = 1,l = c(0.5,0.5),g = matrix(0.25,nrow = 2,ncol = 2),divergence =  'binary_KL_divergence',
+            test_list =   test_list,treatment =  treatment_list,control = 'control',target = 'visit',
+            temp_data = email)
 
--- MainScript.R
+++ MainScript.R
@@ -3,4 +3,11 @@
 
 #Data import
 email <- read.csv('Email.csv')
+email$men_treatment <- ifelse(email$segment=='Mens E-Mail',1,0)
+email$women_treatment <- ifelse(email$segment=='Womens E-Mail',1,0)
+email$control <- ifelse(email$segment=='No E-Mail',1,0)
+email$segment <- NULL
+email$mens <- as.factor(email$mens)
+email$womens <- as.factor(email$womens)
+email$newbie <- as.factor(email$newbie)
 insurance <- read.csv('Insurance.csv')
