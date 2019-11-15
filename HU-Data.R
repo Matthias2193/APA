5b1d76ad0e0c12ae01132de6f7c16b2a8197ed9b
Matthias2193 <matthias.becher2193@gmail.com>
2019-11-15 12:05:51 +0100

    New implementation for some evaluation methods

--- HU-Data.R
+++ HU-Data.R
@@ -20,6 +20,18 @@ tempfunction <- function(x){
 }
 
 hu_data$checkoutAmount <- as.numeric(lapply(as.character(hu_data$checkoutAmount), tempfunction))
+hu_data$Number.of.seconds.between.last.and.previous.views <- 
+  as.numeric(lapply(as.character(hu_data$Number.of.seconds.between.last.and.previous.views), tempfunction))
+for (x in grep("^log.of",colnames(hu_data))) {
+  hu_data[,x] <- as.numeric(lapply(as.character(hu_data[,x]), tempfunction))
+}
+
+for (x in colnames(train[,16:155])) {
+  if(is.na(mean(hu_data[[x]]))){
+    print(x)
+  }
+}
+
 response <- 'checkoutAmount'
 control <- '0'
 
@@ -45,12 +57,10 @@ test_list <- set_up_tests(train[,colnames(train[,16:155])],TRUE)
 
 
 raw_tree <- build_tree(train,0,100,treatment_list,response,control,test_list,criterion = 2)
+pruned_tree <- simple_prune_tree(raw_tree,val,treatment_list,test_list,response,control)
 
-for (x in colnames(hu_data)) {
-  if(is.na(mean(hu_data[[x]]))){
-    print(x)
-  }
-}
+forest <- parallel_build_forest(train,val,treatment_list,response,'0',n_trees = 50,n_features = 100,criterion = 2, pruning = F)
+
 mean(hu_data[[treatment_list[1]]])
 typeof(hu_data[treatment_list[1]][0])
 mean(hu_data["log.of.SecondsSinceOn.about."])
